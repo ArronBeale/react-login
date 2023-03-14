cef4004fb395563c3382b5673a2c6083841dc562
Arron <beale86@msn.com>
2023-03-14 20:26:38 +0000

    Optimise form validation for checking validity

--- react-login/src/components/Login/Login.js
+++ react-login/src/components/Login/Login.js
@@ -50,11 +50,14 @@ const Login = (props) => {
 
   }, []);
 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log('checking...');
       setFormIsValid(
-        emailState.isValid && passwordState.isValid
+        emailIsValid && passwordIsValid
       );
     }, 500);
 
@@ -63,7 +66,7 @@ const Login = (props) => {
       clearTimeout(identifier);
     };
 
-  }, [emailState, passwordState]);
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
